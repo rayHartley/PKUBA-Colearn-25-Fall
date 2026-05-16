00c6059a8b9a686072968a731a829d2ef50919d4
Ren Jiwu <2401210251@stu.pku.edu.cn>
2026-05-16 15:12:03 +0800

    Delete Onchain-data/RenJW418/Liquidation_Monitor directory

--- Onchain-data/RenJW418/Liquidation_Monitor/src/liquidation_sentinel/oracle.py
+++
@@ -1,7 +0,0 @@
-from web3 import Web3
-from .abi import ORACLE_ABI
-
-
-def get_oracle_price(w3: Web3, oracle_address: str, block_number: int) -> int:
-    oracle = w3.eth.contract(address=oracle_address, abi=ORACLE_ABI)
-    return oracle.functions.price().call(block_identifier=block_number)
